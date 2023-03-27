655b2ec9f8d0a4bc13a9f0b971e92d4390342b16
Yoosufsiddiqui <75044619+Yoosufsiddiqui@users.noreply.github.com>
2023-03-26 22:02:06 -0400

    Update CurrentRates.jsx

--- src/components/CurrentRates.jsx
+++ src/components/CurrentRates.jsx
@@ -4,9 +4,9 @@ function CurrentRates({currencyData}) {
     const GBP = currencyData.GBP.rate_float.toFixed(3)
   
     return <div>
-      <h2> 1 BTC = {USD} $ (USD) </h2>
-      <h2> 1 BTC = {EUR} € (EUR) </h2>
-      <h2> 1 BTC = {GBP} £ (GBP) </h2>
+        <h2>{USD} $ (USD) to 1 BTC , {(1/USD).toFixed(6)} BTC to USD</h2>
+        <h2>{EUR} € (EUR) to 1 BTC , {(1/EUR).toFixed(6)} BTC to EUR</h2>
+        <h2>{GBP} £ (GBP) to 1 BTC , {(1/GBP).toFixed(6)} BTC to GBP</h2>
     </div>
   }
   
